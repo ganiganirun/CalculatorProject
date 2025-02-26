00005fccc38cce4f49d031510ed88989dfefd56c
songgaeun <rkdmsdmqek1128@gmail.com>
2025-02-26 12:15:20 +0900

    develop first commit

--- src/calculator/levelone/Main.java
+++ src/calculator/levelone/Main.java
@@ -2,6 +2,6 @@ package calculator.levelone;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("계산기실습");
+        System.out.println("계산기 실습");
     }
 }
