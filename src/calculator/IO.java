7844e0e7bf894d0e7c7e34d2cf61a685d7261a79
songgaeun <rkdmsdmqek1128@gmail.com>
2025-02-27 16:59:19 +0900

    level2 update

--- src/calculator/IO.java
+++ src/calculator/IO.java
@@ -1,6 +1,36 @@
 package calculator;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class IO {
+
+    Scanner scn = new Scanner(System.in);
+    public double[] inputNumArr(double[] numArr){
+        while (true) {
+            try {
+                System.out.print("양의 정수를 두개 입력하시오. ");
+
+                for (int i = 0; i < numArr.length; i++) {
+                    numArr[i] = scn.nextInt();
+                }
+                scn.nextLine(); // 버퍼비우기
+                break;
+            }catch (InputMismatchException e){
+                System.out.println("양의 정수 이외의 값은 입력할 수 없습니다.");
+                scn.nextLine(); // 애로 해도 무한루프 탈출 가능함 -> 근데 얘가 더 효율적
+            }
+        }
+        return numArr;
+    }
+
+    public char inputOper(){
+        System.out.print("+,-,*,/ 중 하나를 입력하시오.");
+        char oper = scn.next().charAt(0);
+        scn.nextLine(); // 버퍼비우기
+
+        return oper;
+    }
 
     public void printResult(double num1, double num2, char oper, double result){
         // equals()은 string 함수라 char 비교시에는 사용 불가능
@@ -15,4 +45,15 @@ public class IO {
         }
 
     }
+
+    public boolean exitCalculator(boolean flag){
+        System.out.print("졸료를 원할 시 exit를 입력하시오. 아닐 시 enter를 누르시오.");
+        String ex = scn.nextLine();
+        if (ex.equals("exit")){
+            flag = false;
+        }
+        return flag;
+    }
+
+
 }
--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -5,63 +5,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
         // while 종료 조건 변수
         boolean flag = true;
         Calculator calculator = new Calculator();
+        IO io = new IO();
 
         while (flag){
             double [] numArr = new double[2];
             double result = 0;
 
-            // 정수형이 아닌 것을 입력받을 시 발생하는 오류 예외 처리
-            while (true) {
-//                 // 무한루프 나가기 시도 2
-//                for(int i = 0; i < numArr.length; i++){
-//                    numArr[i] = 0;
-//                }
-
-                try {
-                    System.out.print("양의 정수를 두개 입력하시오. ");
-
-                    for (int i = 0; i < numArr.length; i++) {
-                        numArr[i] = scn.nextInt();
-
-//                        //무한루프 나가기 시도 1
-//                        int a = scn.nextInt();
-//                        numArr[i] = a;
-                    }
-                    break;
-                }catch (InputMismatchException e){
-                    System.out.println("양의 정수 이외의 값은 입력할 수 없습니다.");
-
-                    // while을 통해서 예외 발생시 반복할 때 scanner를 초기화 시켜줘야 무한루프가 안돌아감
-                    // 블로그 정보 참고
-//                    scn = new Scanner(System.in); // 얘로 해도 무한루프 탈출 가능함
-                    scn.nextLine(); // 애로 해도 무한루프 탈출 가능함 -> 근데 얘가 더 효율적
-                }
-            }
-
-            scn.nextLine(); // 버퍼비우기
-
-            // 사칙연산 기호 입력 받기
-            System.out.print("+,-,*,/ 중 하나를 입력하시오.");
-            char oper = scn.next().charAt(0);
-
-            scn.nextLine(); // 버퍼비우기
-
+            numArr = io.inputNumArr(numArr);
+            char oper = io.inputOper();
             calculator.calculator(numArr[0],numArr[1],oper);
+            flag = io.exitCalculator(flag);
 
-            // while 문 종료 조건 부분
-            // ex의 값이 exit와 같으면 flag의 값을 false로 변경하여 while문 종료.
-            System.out.print("졸료를 원할 시 exit를 입력하시오. 아닐 시 enter를 누르시오.");
-            String ex = scn.nextLine();
-            if(ex.equals("exit")){
-                flag = false;
-            }
         }
         System.out.println("계산기를 종료합니다.");
-        // scanner 종료.
-        scn.close();
+
     }
 }
